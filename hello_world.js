13aeba0a08518ebc7bc07ce52bafe7d887117cda
nayoung <rnjsskdud941@naver.com>
2020-01-15 16:00:17 +0900

    server test

--- hello_world.js
+++ hello_world.js
@@ -1,7 +1,8 @@
 var http = require('http');
 
 http.createServer(function handler(req, res) {
-    res.writeHead (200, {'Content-type': 'text/plain'});
+    res.writeHead (200, {'Content-type': 'text/html'});
+    res.write('<h1>Server Test</h1>')
     res.end('Hello World!\n');
 }).listen(1337, '127.0.0.1');
 
